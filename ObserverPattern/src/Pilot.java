6aabdf92afa114b9b1878ce413f778ad9a7ad15b
vgnshiyer <vgnshiyer@gmail.com>
2023-02-24 12:28:12 -0700

    chore: added code for notifying operator and pilot

--- ObserverPattern/src/Pilot.java
+++ ObserverPattern/src/Pilot.java
@@ -3,7 +3,9 @@ public class Pilot extends Person {
 	private Drone drone;
 
 	public void update(Drone drone) {
-		
+		System.out.println("Notification for pilot :");
+		System.out.println("Drone "+drone.getID()+" has entered "+drone.getState()+" state");
+		System.out.println();
 	}
 
 	void assignDrone(Drone drone){
