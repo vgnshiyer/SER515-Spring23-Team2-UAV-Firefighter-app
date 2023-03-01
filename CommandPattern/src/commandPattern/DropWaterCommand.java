4bf40ad04bf8bc7e4031071e7a12f297e57c22d2
DharaniChinta <nvchinta@asu.edu>
2023-03-01 13:12:24 -0700

    refactor: added method definations for DropWaterCommand Class

--- CommandPattern/src/commandPattern/DropWaterCommand.java
+++ CommandPattern/src/commandPattern/DropWaterCommand.java
@@ -5,11 +5,11 @@ public class DropWaterCommand implements Command {
 	private Drone drone;
 
 	public DropWaterCommand(Drone drone) {
-
+		this.drone = drone;
 	}
 
 	public void execute() {
-
+		drone.dropWater();
 	}
 
 }
