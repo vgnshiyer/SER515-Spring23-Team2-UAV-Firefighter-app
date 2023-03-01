5e4f2c05213b442f0f4fc00cc061c41e364ca1c8
DharaniChinta <nvchinta@asu.edu>
2023-03-01 13:13:43 -0700

    refactor: added method definations for Pilot Class

--- CommandPattern/src/commandPattern/Pilot.java
+++ CommandPattern/src/commandPattern/Pilot.java
@@ -5,11 +5,11 @@ public class Pilot {
 	private Command command;
 
 	public void setCommand(Command command) {
-
+		this.command = command;
 	}
 
 	public void executeCommand() {
-
+		command.execute();
 	}
 
 }
