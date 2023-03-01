8ba6e0a6ac37299318760ff9264848b0374c5e16
DharaniChinta <nvchinta@asu.edu>
2023-03-01 13:09:03 -0700

    refactor: updated the method definations for Drone Class

--- CommandPattern/src/commandPattern/Drone.java
+++ CommandPattern/src/commandPattern/Drone.java
@@ -5,15 +5,15 @@ public class Drone {
 	private String name;
 
 	public Drone(String name) {
-
+		this.name = name;
 	}
 
 	public void move(int x, int y) {
-
+		System.out.println(name + " is moving to (" + x + ", " + y + ")");
 	}
 
 	public void dropWater() {
-
+		System.out.println(name + " is dropping water");
 	}
 
 }
