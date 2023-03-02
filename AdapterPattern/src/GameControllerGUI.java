2d70191334956f97794f4517b89090bf57f212ff
Sri Harsha <sriharsha.g15@iiits.in>
2023-03-01 23:47:33 -0700

    code restructured, added print messages

---
+++ AdapterPattern/src/GameControllerGUI.java
@@ -0,0 +1,9 @@
+
+// GameControllerGUI Class
+public class GameControllerGUI {
+public void displayControls() { 
+System.out.println("Click here to control drone");
+System.out.println("Click here to drop retardants");
+System.out.println("Click here to return to meet");
+ }
+}
---
+++ AdapterPattern/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+/**
+ * @param <ComputerGUI>
+ * @param <iPadGUI>
+ * @param <GameControllerGUI>
+ * @param args
+ */
+public static <ComputerGUI, iPadGUI, GameControllerGUI> void main(String[] args) {
+    // Instantiate Admin, Operator, and Pilot objects
+
+    // Instantiate GUI objects
+    final ComputerGUI computerGUI = new ComputerGUI();
+    final iPadGUI iPadGUI = new iPadGUI();
+    final GameControllerGUI gameControllerGUI = new GameControllerGUI();
+
+    Object operator;
+    // Instantiate Adapters for Operator and Pilot
+    final Adapter operatorAdapter = new Adapter(operator);
+    Object pilot;
+    final Adapter pilotAdapter = new Adapter(pilot);
+    Object admin;
+    final Adapter adminAdapter = new Adapter(admin);
+
+    // Use Adapters to control drones
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter 0 to enter into Admin interface");
+    System.out.println("Enter 1 to control drone as Operator");
+    System.out.println("Enter 2 to control drone as Pilot");
+    int choice = scanner.nextInt();
+    switch (choice) {
+        case 0:
+            ComputerGUI.displayMission();
+            break;
+        case 1:
+            iPadGUI.displayMission();
+            operatorAdapter.controlDrone();
+            break;
+        case 2:
+            GameControllerGUI.displayControls();
+            pilotAdapter.controlDrone();
+            break;
+        default:
+            System.out.println("Invalid choice");
+            break;
+    }
+}
+}
+   
