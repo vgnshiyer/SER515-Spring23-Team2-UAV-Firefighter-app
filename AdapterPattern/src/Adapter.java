118edad6c21b45b76aa487c3899a54125cf2b506
Sri Harsha <sriharsha.g15@iiits.in>
2023-03-02 00:49:55 -0700

    minor changes

--- AdapterPattern/src/Adapter.java
+++ AdapterPattern/src/Adapter.java
@@ -1,4 +1,6 @@
 // Adapter Interface
+
+
 public interface Adapter {
     public void controlDrone();
     }    
--- AdapterPattern/src/GameControllerGUI.java
+++ AdapterPattern/src/GameControllerGUI.java
@@ -1,7 +1,7 @@
 
 // GameControllerGUI Class
 public class GameControllerGUI {
-public static void displayControls() { 
+public void displayControls() { 
 System.out.println("Click here to control drone");
 System.out.println("Click here to drop retardants");
 System.out.println("Click here to return to meet");
