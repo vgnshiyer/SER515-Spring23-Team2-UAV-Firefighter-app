6ba3edf6bb481062ff3743a2460a137ad3fb5efc
Chaitya Sanghavi <chaityasanghavi33@gmail.com>
2023-03-02 01:32:30 -0700

    Added drone to the pack satisfying singleton pattern definition

--- Singleton_Pattern/main.java
+++ Singleton_Pattern/main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
         Pack pack = Pack.getInstance();
+        Drone drone1 = new Drone(1);
+        pack.addDrone(drone1);
+
+        Drone drone2 = new Drone(2);
+        pack.addDrone(drone2);
     }
 }
