5b2918343fae6d734d8123f70c80a7279afd3345
Sri Harsha <sriharsha.g15@iiits.in>
2023-03-01 20:59:34 -0700

    facilitate conditions for interfaces

--- AdapterPattern/main.java
+++ AdapterPattern/main.java
@@ -13,5 +13,10 @@ public static void main(String[] args) {
     Adapter pilotAdapter = new AdapterImpl(pilot);
 
     // Use Adapters to control drones
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter 0 to enter into Admin interface");
+    System.out.println("Enter 1 to control drone as Operator");
+    System.out.println("Enter 2 to control drone as Pilot");
+    int choice = scanner.nextInt();
     operatorAdapter.controlDrone();
    
