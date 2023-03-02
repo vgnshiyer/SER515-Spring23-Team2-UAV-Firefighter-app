e8828a1bd294db915f05566be1e99fd7d581db6b
vgnshiyer <vgnshiyer@gmail.com>
2023-03-02 02:40:13 -0700

    chore: FINAL COMMIT

---
+++ ObserverPattern/src/main.java
@@ -0,0 +1,53 @@
+public class main {
+    public static void main(String[] args){
+        Drone drone1 = new Drone(1,1);
+        Drone drone2 = new Drone(2,1);
+        Drone drone3 = new Drone(1,2);
+
+        Operator operator1 = new Operator(1);
+        Operator operator2 = new Operator(2);
+        Pilot pilot1 = new Pilot(3);
+
+        // registering person data in drones
+        /*
+         * drone1 - pack1 - operator1
+         * drone2 - pack1 - operator1
+         * drone1 - pack1 - pilot1
+         * 
+         * drone3 - pack2 - operator2
+         */
+        drone1.addPerson(operator1);
+        drone2.addPerson(operator1);
+        drone1.addPerson(pilot1);
+
+        drone3.addPerson(operator2);
+        
+        // registering drone data to operators and pilots - although not reqd for this demonstration
+        operator1.addDrone(drone1);
+        operator1.addDrone(drone2);
+        pilot1.assignDrone(drone1);
+
+        operator2.addDrone(drone3);
+
+        // adding dummy states for the drone
+        Drone.valid_states.add("take-off");
+        Drone.valid_states.add("join-pack");
+        Drone.valid_states.add("disband");
+        Drone.valid_states.add("malfunction");
+        Drone.valid_states.add("exhausted-resources");
+
+        // demonstrating state change for drones
+        for(int i = 0; i < 3; i++){
+            int droneid = getDroneID(1,3);
+            Drone drone = null;
+            if(droneid == 1) drone = drone1;
+            else if(droneid == 2) drone = drone2;
+            else drone = drone3;
+            drone.changeState();
+        }
+    }
+
+    public static int getDroneID(int min, int max){
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+}
