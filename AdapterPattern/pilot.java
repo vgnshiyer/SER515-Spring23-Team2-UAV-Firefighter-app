cddbb6f71a60b1e10158ce47ae2ad775bc5f2063
Sri Harsha <sriharsha.g15@iiits.in>
2023-03-01 20:25:32 -0700

    restructured code

---
+++ AdapterPattern/pilot.java
@@ -0,0 +1,6 @@
+/ Pilot Class
+public class Pilot {
+public void controlDrone() { /* implementation */ }
+public void dropRetardants() { /* implementation */ }
+public void returnToMeet() { /* implementation */ }
+}
